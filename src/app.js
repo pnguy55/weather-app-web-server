9a9fe29551532d168476f7bbaaccaf7952658e7e
Phi Nguyen <pnguy55@gmail.com>
2019-10-08 11:06:10 -0500

    add a footer and 404 pages, edit style

--- src/app.js
+++ src/app.js
@@ -17,27 +17,53 @@ hbs.registerPartials(partialsPath)
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
+//Some variables
+let author = 'Created by Phi Nguyen'
+
+
 app.get('', (req,res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'Phi Nguyen'
+        name: author
     })
 })
 
 app.get('/about', (req,res) => {
     res.render('about', {
         title: 'Purpose of the Weather App',
-        name: 'Phi Nguyen'
+        name: author
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'FAQ',
-        nam: 'Phi Nguyen',
+        name: author,
         helpText: 'This is some helpful text'
     })
 })
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philidelphia'
+    })
+})
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Help 404',
+        bodyOf404: 'Help article not found',
+        name: author
+    })
+})
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        bodyOf404: 'This is my 404 page',
+        name: author
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
